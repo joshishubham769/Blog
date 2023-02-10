7c7009a532dd7707936d2af55873ae9d269a23b4
joshishubham769 <joshishubham769@gmail.com>
2023-02-10 14:47:38 +0530

    added ExceptionHandlerClass

---
+++ src/main/java/com/blog/Blog/exceptionHandler/ExceptionHandlerClass.java
@@ -0,0 +1,14 @@
+package com.blog.Blog.exceptionHandler;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerClass {
+
+    //@ExceptionHandler(value = RuntimeException.class)
+//    public ResponseEntity<Object> exception(ParticularException exception) {
+//    }
+}
